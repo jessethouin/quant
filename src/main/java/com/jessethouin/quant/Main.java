638868c1fd7d3272e71562d29bb78f245b2464c2
Jesse Thouin <github-jt23@sneakemail.com>
2020-09-25 22:23:36 -0400

    Added timing. 'tis sloppy, need to use aspects.

--- src/main/java/com/jessethouin/quant/Main.java
+++ src/main/java/com/jessethouin/quant/Main.java
@@ -1,13 +1,12 @@
 package com.jessethouin.quant;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.time.StopWatch;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.InputStream;
 import java.math.BigDecimal;
-import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.text.MessageFormat;
 import java.util.*;
 import java.util.concurrent.*;
@@ -21,21 +20,27 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         StopWatch watch = new StopWatch();
-        StopWatch loopWatch = new StopWatch();
         watch.start();
 
-        int max = 30;
-        BigDecimal rmax = BigDecimal.valueOf(.25);
-        BigDecimal hlIncrement = BigDecimal.valueOf(.05);
+        int start = 0;
+        int max = 10;
+        BigDecimal rmax = BigDecimal.valueOf(.10);
+        BigDecimal hlIncrement = BigDecimal.valueOf(.01);
 
-        if (args.length == 0) LOG.info("Using default values for max (30), rmax (.25), and hlIncrement (.05).");
+        if (args.length == 0) LOG.info(MessageFormat.format("Using default values for start {0}, max {1}, rmax {2}, and hlIncrement {3}.", start, max, rmax, hlIncrement));
 
         for (int i = 0; i < args.length; i++) {
             switch (i) {
-                case 0 -> max = Integer.parseInt(args[i]);
-                case 1 -> rmax = BigDecimal.valueOf(Double.parseDouble(args[i]));
-                case 2 -> hlIncrement = BigDecimal.valueOf(Double.parseDouble(args[i]));
+                case 0 -> start = Integer.parseInt(args[i]);
+                case 1 -> max = Integer.parseInt(args[i]);
+                case 2 -> rmax = BigDecimal.valueOf(Double.parseDouble(args[i]));
+                case 3 -> hlIncrement = BigDecimal.valueOf(Double.parseDouble(args[i]));
             }
+        }
+
+        if (start > max) {
+            LOG.error(MessageFormat.format("Starting moving average count {0} must be less than max {1}", start, max));
+            return;
         }
 
 /*
@@ -50,22 +55,37 @@ public class Main {
 
         populateIntradayPrices();
 
+        int combos = getCombos(start, max, rmax, hlIncrement);
+
+        LOG.info(MessageFormat.format("{0} combinations were tested.", combos));
+        LOG.info(MessageFormat.format("\n\nThe best combination of parameters is\n\t{0}\nwith a value of ${1}\n", getBest(), getBestv()));
+
+        watch.stop();
+        LOG.info(MessageFormat.format("Time Elapsed: {0}", watch.getTime(TimeUnit.MILLISECONDS) / 1000));
+
+//        ImageCharts line = new ImageCharts().cht("ls").chd()
+    }
+
+    private static int getCombos(int start, int max, BigDecimal rmax, BigDecimal hlIncrement) throws InterruptedException {
         int s;
         int l = 0;
+        int c = 0;
+        int i = ((max * (max + 1)) / 2) - max;
+        int i_ = i;
+        double secondsLeft = 0;
         BigDecimal rh;
         BigDecimal rl;
         int combos = 0;
-        long nt = 0;
-        List<BigInteger> nt_ = new ArrayList<>();
+        long nt;
+        List<Long> nt_ = new ArrayList<>();
+        StopWatch loopWatch = new StopWatch();
 
-        while (l < max) {
-            l++;
-            s = 0;
-            while (s < l) {
-                s++;
+        while (++l <= max) {
+            s = start;
+            while (++s < l) { // there's no need to test short and long tail MAs because they will never separate or converge.
                 rh = BigDecimal.ZERO;
+                loopWatch.start();
                 while (rh.compareTo(rmax) < 0) {
-                    loopWatch.start();
                     rh = rh.add(hlIncrement);
                     rl = BigDecimal.ZERO;
                     ExecutorService es = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
@@ -75,26 +95,30 @@ public class Main {
                         rl = rl.add(hlIncrement);
                         todo.add(new ProcessHistoricIntradayPrices(s, l, rh, rl, intradayPrices));
                     }
-                    List<Future<Object>> answers = es.invokeAll(todo);
+                    es.invokeAll(todo);
                     es.shutdown();
                     es.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-                    loopWatch.stop();
-                    nt = loopWatch.getNanoTime();
-                    nt_.add(BigInteger.valueOf(nt));
-                    loopWatch.reset();
                 }
-                OptionalDouble ntAvg = nt_.stream().mapToLong(BigInteger::longValue).average();
-                LOG.info(MessageFormat.format("Average execution time for threads is {0} seconds.", ntAvg.isPresent() ? ntAvg.getAsDouble() / 1000000000 : 0));
+                loopWatch.stop();
+                nt = loopWatch.getTime(TimeUnit.MILLISECONDS);
+                nt_.add(nt);
+                loopWatch.reset();
+
+                secondsLeft = secondsLeft == 0 ? nt / (double)1000 : secondsLeft;
+                double ntAvg = nt_.stream().mapToLong(a -> a).average().orElse(0);
+                double r = (ntAvg / (double)1000) * --i_;
+                r = r % (24 * 3600);
+                double hour = r / 3600;
+                r %= 3600;
+                double minutes = r / 60 ;
+                r %= 60;
+                double seconds = r;
+
+                LOG.debug(MessageFormat.format("Average execution time for {3} threads (s:{1} l:{2}) is {0} seconds per thread.", nt, s, l, rmax.divide(hlIncrement, RoundingMode.HALF_UP)));
+                LOG.info(MessageFormat.format("{0,number,percent} complete. {1,number,00:}{2,number,00:}{3,number,00.00} seconds remaining", ++c / (float) i, hour, minutes, seconds));
             }
         }
-
-        LOG.info(MessageFormat.format("{0} combinations were tested.", combos));
-        LOG.info(MessageFormat.format("\n\nThe best combination of parameters is\n\t{0}\nwith a value of ${1}\n", getBest(), getBestv()));
-
-        watch.stop();
-        LOG.info(MessageFormat.format("Time Elapsed: {0}", watch.getTime(TimeUnit.MILLISECONDS) / 1000));
-
-//        ImageCharts line = new ImageCharts().cht("ls").chd()
+        return combos;
     }
 
     private static void populateIntradayPrices() {
